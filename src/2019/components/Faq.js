bad8ca80f73f24841727eb532722e90e6ada787e
Lisa Luo <llluo@ucsd.edu>
2020-02-18 10:31:06 -0800

    faq

--- src/2019/components/Faq.js
+++ src/2019/components/Faq.js
@@ -9,51 +9,12 @@ import {
 } from '../styles';
 
 import {FaqConstants} from '../constants';
-
 import Question from './Question';
 import faqData from '../data/Faq';
-
-const FaqQuestion = styled.h3`
-    font-weight: bold;
-    color: white;
-    margin-bottom: 0.5rem;
-`
-
-const FaqAnswer = styled.p`
-    color: white;
-    margin-bottom: 0.1rem;
-`
-
-const FaqBox = styled.div`
-    background: rgba(255, 255, 255, 0.2);
-    border-radius: 45px;
-    padding: 1.5rem;
-    margin-top: 1.5rem;
-    margin-bottom: 1.5rem;
-    vertical-align: middle;
-`
-
-const FaqLink = styled.a`
-    color: white;
-    text-decoration: underline;
-
-    &:hover {
-        color: #094E6E;
-    }
-`
-
-const Stingray = styled.img`
-    margin-top: 1rem;
-    width: 90%;
-`
 
 const FaqSection = styled(Page)`
     background: ${FaqConstants.gradient};
     height: auto;
-`
-
-const FaqContactLink = styled.a`
-    color: white !important;
 `
 
 class Faq extends Component {
